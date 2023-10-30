3ecf42c9a06a6ea1aea0778eb8df360adef5d84d
ibl4zqu3z <ibl4zqu3z@protonmail.com>
2023-10-30 10:01:28 +0100

    obtencion de perfil

--- src/dame_datos.py
+++ src/dame_datos.py
@@ -40,3 +40,73 @@ with open(nombre_archivo, "w") as archivo:
             archivo.write(f"{clave}: {valor}\n")
 
 print(f"Datos personales guardados en {nombre_archivo}")
+
+
+def interactive():
+    """Implementation of the -i switch. Interactively question the user and
+    create a password dictionary file based on the answer."""
+
+    print("\r\n[+] Insert the information about the victim to make a dictionary")
+    print("[+] If you don't know all the info, just hit enter when asked! ;)\r\n")
+
+    # We need some information first!
+
+    profile = {}
+
+    name = input("> First Name: ").lower()
+    while len(name) == 0 or name == " " or name == "  " or name == "   ":
+        print("\r\n[-] You must enter a name at least!")
+        name = input("> Name: ").lower()
+    profile["name"] = str(name)
+
+    profile["surname"] = input("> Surname: ").lower()
+    profile["nick"] = input("> Nickname: ").lower()
+    birthdate = input("> Birthdate (DDMMYYYY): ")
+    while len(birthdate) != 0 and len(birthdate) != 8:
+        print("\r\n[-] You must enter 8 digits for birthday!")
+        birthdate = input("> Birthdate (DDMMYYYY): ")
+    profile["birthdate"] = str(birthdate)
+
+    print("\r\n")
+
+    profile["wife"] = input("> Partners) name: ").lower()
+    profile["wifen"] = input("> Partners) nickname: ").lower()
+    wifeb = input("> Partners) birthdate (DDMMYYYY): ")
+    while len(wifeb) != 0 and len(wifeb) != 8:
+        print("\r\n[-] You must enter 8 digits for birthday!")
+        wifeb = input("> Partners birthdate (DDMMYYYY): ")
+    profile["wifeb"] = str(wifeb)
+    print("\r\n")
+
+    profile["kid"] = input("> Child's name: ").lower()
+    profile["kidn"] = input("> Child's nickname: ").lower()
+    kidb = input("> Child's birthdate (DDMMYYYY): ")
+    while len(kidb) != 0 and len(kidb) != 8:
+        print("\r\n[-] You must enter 8 digits for birthday!")
+        kidb = input("> Child's birthdate (DDMMYYYY): ")
+    profile["kidb"] = str(kidb)
+    print("\r\n")
+
+    profile["pet"] = input("> Pet's name: ").lower()
+    profile["company"] = input("> Company name: ").lower()
+    print("\r\n")
+
+    profile["words"] = [""]
+    words1 = input(
+        "> Do you want to add some key words about the victim? Y/[N]: "
+    ).lower()
+    words2 = ""
+    if words1 == "y":
+        words2 = input(
+            "> Please enter the words, separated by comma. [i.e. hacker,juice,black], spaces will be removed: "
+        ).replace(" ", "")
+    profile["words"] = words2.split(",")
+
+    profile["spechars1"] = input(
+        "> Do you want to add special chars at the end of words? Y/[N]: "
+    ).lower()
+
+    profile["randnum"] = input(
+        "> Do you want to add some random numbers at the end of words? Y/[N]:"
+    ).lower()
+    profile["leetmode"] = input("> Leet mode? (i.e. leet = 1337) Y/[N]: ").lower()
--- src/nuevomenu.py
+++ src/nuevomenu.py
@@ -1,6 +1,77 @@
 import random
 import string
 
+def interactive():
+    """Implementation of the -i switch. Interactively question the user and
+    create a password dictionary file based on the answer."""
+
+    print("\r\n[+] Introduce informacion sobre la persona para hacer un diccionario de fuerza bruta basado en su perfil")
+    print("[+] Si no sabes algun dato, pulsa intro para saltar a la siguiente pregunta.\r\n")
+
+    # We need some information first!
+
+    profile = {}
+
+    nombre = input("> First nombre: ").lower()
+    while len(nombre) == 0 or nombre == " " or nombre == "  " or nombre == "   ":
+        print("\r\n[-] You must enter a nombre at least!")
+        nombre = input("> nombre: ").lower()
+    profile["nombre"] = str(nombre)
+
+    profile["apellido"] = input("> apellido: ").lower()
+    profile["nick"] = input("> Nick: ").lower()
+    fecha_nacimiento = input("> Fecha de Nacimiento (DDMMYYYY): ")
+    while len(fecha_nacimiento) != 0 and len(fecha_nacimiento) != 8:
+        print("\r\n[-] Debes ingresar 8 dígitos para el cumpleaños.!")
+        fecha_nacimiento = input("> Fecha de nacimiento (DDMMYYYY): ")
+    profile["fecha_nacimiento"] = str(fecha_nacimiento)
+
+    print("\r\n")
+
+    profile["mujer"] = input("> Conyuge / pareja / novi@ nombre: ").lower()
+    profile["mujern"] = input("> Conyuge / pareja / novi@ nickname: ").lower()
+    mujerb = input("> Conyuge / pareja / novi@ Fecha de Nacimiento (DDMMYYYY): ")
+    while len(mujerb) != 0 and len(mujerb) != 8:
+        print("\r\n[-] Debes ingresar 8 dígitos para el cumpleaños.!")
+        mujerb = input("> Conyuge / pareja / novi@ Fecha de Nacimiento (DDMMYYYY): ")
+    profile["mujerb"] = str(mujerb)
+    print("\r\n")
+
+    profile["kid"] = input("> Child's nombre: ").lower()
+    profile["kidn"] = input("> Child's nickname: ").lower()
+    kidb = input("> Child's fecha_nacimiento (DDMMYYYY): ")
+    while len(kidb) != 0 and len(kidb) != 8:
+        print("\r\n[-] You must enter 8 digits for birthday!")
+        kidb = input("> Child's fecha_nacimiento (DDMMYYYY): ")
+    profile["kidb"] = str(kidb)
+    print("\r\n")
+
+    profile["pet"] = input("> Pet's nombre: ").lower()
+    profile["company"] = input("> Company nombre: ").lower()
+    print("\r\n")
+
+    profile["words"] = [""]
+    words1 = input(
+        "> Do you want to add some key words about the victim? Y/[N]: "
+    ).lower()
+    words2 = ""
+    if words1 == "y":
+        words2 = input(
+            "> Please enter the words, separated by comma. [i.e. hacker,juice,black], spaces will be removed: "
+        ).replace(" ", "")
+    profile["words"] = words2.split(",")
+
+    profile["spechars1"] = input(
+        "> Do you want to add special chars at the end of words? Y/[N]: "
+    ).lower()
+
+    profile["randnum"] = input(
+        "> Do you want to add some random numbers at the end of words? Y/[N]:"
+    ).lower()
+    profile["leetmode"] = input("> Leet mode? (i.e. leet = 1337) Y/[N]: ").lower()
+
+    print(profile)
+
 def leet_speak_vocals(input_text):
     leet_dict = {
         'A': '4',
@@ -49,7 +120,7 @@ def generate_random_password(length):
     return password
 
 def display_banner():
-    print("    -... -.--     .. -... .-.. ....- --.. --.- ..- ...-- --.. ")
+    print("        .. -... .-.. ....- --.. --.- ..- ...-- --..            ")
     print(" ________  ________  ________   ________  ________  ___    ___ ")
     print("|\   __  \|\   __  \|\   ____\ |\   ____\|\   __  \|\  \  /  /| ")
     print("\ \  \|\  \ \  \|\  \ \  \___|_\ \  \___|\ \  \|\  \ \  \/  / |")
@@ -59,7 +130,6 @@ def display_banner():
     print("    \|__|     \|__|\|__|\_________\\_________\|__||\___/ /     ")
     print("                       \|_________\|_________|    \|___|/    ")
     print("")
-    print("-... -.--     .. -... .-.. ....- --.. --.- ..- ...-- --.. ")
     print(" _               ____  _ _     _ _  _                 _____    ")
     print("| |__  _   _    / __ \(_) |__ | | || | ______ _ _   _|___ /____")
     print("| '_ \| | | |  / / _` | | '_ \| | || ||_  / _` | | | | |_ \_  /")
@@ -67,7 +137,7 @@ def display_banner():
     print("|_.__/ \__, |  \ \__,_|_|_.__/|_|  |_|/___\__, |\__,_|____/___|")
     print("       |___/    \____/                       |_|               ")
     print("")
-    print("-... -.--     .. -... .-.. ....- --.. --.- ..- ...-- --.. ")
+    print("        .. -... .-.. ....- --.. --.- ..- ...-- --..            ")
     print("\n\n")
 def main_menu():
     while True:
@@ -77,7 +147,7 @@ def main_menu():
         print("1. Generar contraseña aleatoria")
         print("2. Transformar texto a lenguaje leet")
         print("3. Transformar texto a lenguaje leet solo vocales")
-        print("4. Opción 4")
+        print("4. Crear Diccionario desde perfil")
         print("\n0. Salir")
 
         choice = input("\nSeleccione una opción (1/2/3/4): ")
@@ -97,7 +167,9 @@ def main_menu():
             leet_text = leet_speak_vocals(text)
             print("\nTexto en lenguaje leet:\t", leet_text)
             input("\nPresione Enter para volver al menú principal.")
-        
+        elif choice == "4":
+            interactive()
+            input("\nPresione Enter para volver al menú principal.")
         elif choice == "0":
             print("Saliendo del programa. ¡Hasta luego!")
             break
@@ -106,3 +178,5 @@ def main_menu():
 
 if __name__ == "__main__":
     main_menu()
+
+
